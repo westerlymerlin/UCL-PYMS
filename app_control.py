65fcf1956c772920f076413b8f4fdf0810b39fee
Gary Twinn <gary@twinn.net>
2025-02-16 17:28:41 +0000

    3.2.36  Change to the font colour on Ncc Form

--- app_control.py
+++ app_control.py
@@ -7,7 +7,7 @@ calculating a file name and removing illegal character.
 import json
 import datetime
 
-VERSION = '3.2.35'
+VERSION = '3.2.36'
 running = True
 alarms = {'laserhost': 0, 'valvehost': 0, 'xyhost': 0, 'pumphost': 0, 'hidenhost': 0, 'laseralarm': 133}
 
